36eea5420a6a7cbfd507a97fb6246ba6ee2f109f
TheGreekBrit <howdy140@gmail.com>
2016-12-19 20:46:25 -0500

    added "prefix" prop to instructions, dictates things like using #$ for constants and $ for others

--- dis.py
+++ dis.py
@@ -16,23 +16,23 @@ try:
         if(op['length'] == 2):
             arg_bytes = rom.read(1)
             arg_bytes_hex = struct.unpack('<B', arg_bytes)[0]
-            arg = '${0:02x}'.format(arg_bytes_hex)
+            arg = '{0:02x}'.format(arg_bytes_hex)
         elif(op['length'] == 3):
             arg_bytes = rom.read(2)
             arg_bytes_hex = struct.unpack('<BB', arg_bytes)
             arg_bytes_hex = int(''.join(str(i) for i in arg_bytes_hex))
-            arg = '${0:04x}'.format(arg_bytes_hex)
+            arg = '{0:04x}'.format(arg_bytes_hex)
         elif(op['length'] == 4):
             arg_bytes = rom.read(3)
             arg_bytes_hex = struct.unpack('<BBB', arg_bytes)
 
             arg_bytes_hex = int(''.join(str(i) for i in arg_bytes_hex))
-            arg = '${0:06x}'.format(arg_bytes_hex)
+            arg = '{0:06x}'.format(arg_bytes_hex)
 
         #output.write('${0:06x}: '.format(address) + hex(byte_hex[0]))
         #print '${0:06x}: '.format(address) + hex(byte_hex[0])
         if arg_bytes_hex != None:
-            print '${0:06x}: '.format(address) + op['opcode'] + ' ' + arg
+            print '${0:06x}: '.format(address) + op['opcode'] + ' ' + op['prefix'] + arg
         else:
             print '${0:06x}: '.format(address) + op['opcode']
 
